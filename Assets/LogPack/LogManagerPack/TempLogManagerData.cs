94a3a96fc1fe7aca6a5c252bb4d8686f2aba7633
2C2C2C <a2c2c123@126.com>
2020-05-20 23:19:25 +0800

    add pic

--- Assets/LogPack/LogManagerPack/TempLogManagerData.cs
+++ Assets/LogPack/LogManagerPack/TempLogManagerData.cs
@@ -2,8 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "CustomLog/Log Manager Setting")]
-public class TempLogManagerData : SingletonScriptableObject<TempLogManagerData>
+namespace CustomLog
 {
+    [CreateAssetMenu(menuName = "CustomLog/Log Manager Setting")]
+    public class TempLogManagerData : SingletonScriptableObject<TempLogManagerData>
+    {
+        public bool m_showLog = true;
+        public bool m_showWarning = true;
+        public bool m_showError = true;
 
+        private List<TempLogItem> m_tempLogs = null;
+
+        public void SetTempData(bool[] showFlag, List<TempLogItem> tempLogs)
+        {
+
+        }
+
+        public void GetTempData(out List<TempLogItem> tempLogs)
+        {
+            tempLogs = new List<TempLogItem>(m_tempLogs);
+        }
+
+    }
 }
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # TempUnityLogConsoleClone
 
+![TestImg](FolderToRemove/pic001.jpg)
+
 A simple clone of Unity log console.
 I learn it from : https://gram.gs/gramlog/creating-editor-windows-in-unity/
